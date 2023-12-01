f62c6545e34dac27ce4bd210d68a1c5329412380
Mart Young <mart2great@yahoo.com>
2023-12-01 09:35:33 +0100

    PEEP-24 - create homepage (#6)

--- peepo/settings.py
+++ peepo/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = "peepo.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR /'peepo'/'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -127,6 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    'user_auth/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- user_auth/views.py
+++ user_auth/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
+# def index(request):
+#     return render(request, 'user_auth/home.html', {})
+
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'user_auth/home.html', {})
+
 def welcome_user(request):
 	return HttpResponse("welcome to peepo")
