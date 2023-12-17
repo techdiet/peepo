eef906958ccdc31e90299658a2293db6efaf6e44
enjesh <enjesh@gmail.com>
2023-12-18 02:29:04 +0300

    PEEP-26 - create dashboard

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import welcome_user, HomeView
+from .views import DashboardView
 
 urlpatterns = [
     # path('', HomeView.as_view(), name='index'),
 	path("", welcome_user, name="user_auth"),
+    path("<pk>/", DashboardView.as_view(), name="dashboard"),
 ]
